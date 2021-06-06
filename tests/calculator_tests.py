b9d5fbe3690720a152670288d6f78e5372aab7e0
eren <ebllggithub@gmail.com>
2021-06-06 20:58:23 +0300

    add tests

---
+++ tests/calculator_tests.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from app.calculator import add
+
+import json
+
+
+class CalculatorTests(TestCase):
+    def test_add(self):
+        data = {"num1": 5, "num2": 10}
+        event = {"body": json.dumps(data)}
+
+        resp = add(event, context={})
+
+        self.assertEqual(resp["statusCode"], 200)
+        self.assertEqual(resp["body"], json.dumps({"result": 15}))
