826a1cae3247e48f77e90dbdc6f35f5c8b119b8a
eren <ebllggithub@gmail.com>
2021-06-06 21:04:42 +0300

    dummy changes

--- app/calculator.py
+++ app/calculator.py
@@ -3,9 +3,6 @@ import json
 
 def add(event, context):
     data = json.loads(event["body"])
-
     body = {"result": data["num1"] + data["num2"]}
-
     response = {"statusCode": 200, "body": json.dumps(body)}
-
     return response
