12650fb70b2e6339b6e474c6378b5fc65be3dc2e
eren <ebllggithub@gmail.com>
2021-06-06 20:06:07 +0300

    implement add endpoint

---
+++ calculator.py
@@ -0,0 +1,11 @@
+import json
+
+
+def add(event, context):
+    data = json.loads(event["body"])
+
+    body = {"result": data["num1"] + data["num2"]}
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
