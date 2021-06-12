489a91f1b8eff944bd7cb133fab2b77451fb3a25
eren <ebllggithub@gmail.com>
2021-06-13 01:42:57 +0300

    add security flaws

--- app/calculator.py
+++ app/calculator.py
@@ -1,15 +1,11 @@
 import json
 
 from typing import Dict, Any
+import telnetlib
 
 def add(event: Dict[str, Any], context) -> Dict[str, Any]:
     data: dict = json.loads(event["body"])
     result: int = data["num1"] + data["num2"]
     body: str = json.dumps({"newResult": result})
 
-
-
     return {"statusCode": 200, "body": body}
-
-
-
