0f2020f12f01fc20115c344d468183cb2ac68a0d
eren <ebllggithub@gmail.com>
2021-06-13 01:53:11 +0300

    fix security errors

--- app/calculator.py
+++ app/calculator.py
@@ -1,12 +1,11 @@
 import json
 
 from typing import Dict, Any
-import telnetlib
 
 
 def add(event: Dict[str, Any], context) -> Dict[str, Any]:
     data: dict = json.loads(event["body"])
     result: int = data["num1"] + data["num2"]
     body: str = json.dumps({"newResult": result})
-    eval("2 + 2")
+
     return {"statusCode": 200, "body": body}
