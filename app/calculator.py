61024586d688df4ad9d5e6541fa90dfe5fa98f5c
eren <ebllggithub@gmail.com>
2021-06-13 18:00:55 +0300

    sleep for 1 sec

--- app/calculator.py
+++ app/calculator.py
@@ -1,4 +1,5 @@
 import json
+import time
 from typing import Any, Dict
 
 
@@ -6,5 +7,5 @@ def add(event: Dict[str, Any], context) -> Dict[str, Any]:
     data: dict = json.loads(event["body"])
     result: int = data["num1"] + data["num2"]
     body: str = json.dumps({"sum": result})
-
+    time.sleep(1)
     return {"statusCode": 200, "body": body}
