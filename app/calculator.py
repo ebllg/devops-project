79a96dc9c4f743781e2acfd188b0d38f51e3a9ca
eren <ebllggithub@gmail.com>
2021-06-16 02:41:55 +0300

    remove sleep

--- app/calculator.py
+++ app/calculator.py
@@ -1,6 +1,5 @@
 import json
 import logging
-import time
 from typing import Any, Dict
 
 logger = logging.getLogger(__name__)
@@ -15,5 +14,5 @@ def add(event: Dict[str, Any], context) -> Dict[str, Any]:
 
     result: int = data["num1"] + data["num2"]
     body: str = json.dumps({"sum": result})
-    time.sleep(1)
+
     return {"statusCode": 200, "body": body}
