21fa6d7129d85ff493d80e9a659359db0071338e
eren <ebllggithub@gmail.com>
2021-06-16 01:57:03 +0300

    raise exception

--- app/calculator.py
+++ app/calculator.py
@@ -10,7 +10,7 @@ def add(event: Dict[str, Any], context) -> Dict[str, Any]:
 
     if not isinstance(data["num1"], int) or not isinstance(data["num2"], int):
         logger.error(f"Invalid operands: {data['num1']}, {data['num2']}")
-        return {"statusCode": 400, "body": json.dumps({"error": "Operands must be integers."})}
+        raise Exception("Operands must be integers!")
 
     result: int = data["num1"] + data["num2"]
     body: str = json.dumps({"sum": result})
