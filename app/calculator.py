4c9e959473cc9f4bbcc9b0528e7ee09de106966d
eren <ebllggithub@gmail.com>
2021-06-16 01:38:27 +0300

    add validations

--- app/calculator.py
+++ app/calculator.py
@@ -1,11 +1,18 @@
 import json
-import time
+import logging
 from typing import Any, Dict
+
+logger = logging.getLogger(__name__)
 
 
 def add(event: Dict[str, Any], context) -> Dict[str, Any]:
     data: dict = json.loads(event["body"])
+
+    if not isinstance(data["num1"], int) or not isinstance(data["num2"], int):
+        logger.error(f"Invalid operands: {data['num1']}, {data['num2']}")
+        return {"statusCode": 400, "errorMessage": "Operands must be integers."}
+
     result: int = data["num1"] + data["num2"]
     body: str = json.dumps({"sum": result})
-    time.sleep(1)
+
     return {"statusCode": 200, "body": body}
